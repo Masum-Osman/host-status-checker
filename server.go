f152919bb3b40bee637094ef640c630e5dac46b9
Masum_Osman <masum@trucklagbe.com>
2022-06-22 18:07:35 +0600

    init: just log

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func logging(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		next(w, r)
+	}
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hi, Welcome Web Address!")
+
+	// fs := http.FileServer(http.Dir("./static/example.html"))
+}
+
+func main() {
+	http.HandleFunc("/status", logging(status))
+	http.ListenAndServe(":8084", nil)
+}
